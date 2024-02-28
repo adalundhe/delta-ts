20753e00ccc09df8045a346c31d57910f7478873
Ada Lundhe <ada@datavant.com>
2024-02-28 16:43:28 -0600

    AL: remove useSyncExternalStore from atom to prevent suspense issues

--- src/react/atom.ts
+++ src/react/atom.ts
@@ -1,8 +1,10 @@
-import { useMemo, useRef, useSyncExternalStore } from "react";
+import { useCallback, useMemo, useRef, useState, Dispatch, SetStateAction } from "react";
 import { createAtomApi, getValueFromCreator } from "~/base/atom.ts";
 import { Atom, Listener, Read } from "~/base/types.ts";
 
-const createAtom = <T>(creator: T | Read<T>) => {
+
+
+const createAtom = <T>(creator: T | Read<T>, setter?: Dispatch<SetStateAction<T>>) => {
   const createNextAtom = createAtomApi();
   const atomStore = createNextAtom<T>({} as any);
 
@@ -11,11 +13,12 @@ const createAtom = <T>(creator: T | Read<T>) => {
 
   return {
     store: atomStore,
-    get: (transform?: Read<T>) =>
-      transform ? getValueFromCreator(transform) : atomStore.getState(),
+    get: (transform?: Read<T>) => transform ? getValueFromCreator(transform) : atomStore.getState(),
     set: (next: T | Read<T>) => {
-      const value = getValueFromCreator(next);
+
+      const value = getValueFromCreator(next)
       atomStore.setState(value);
+      setter && setter(value)
       atomStore.subscribers.forEach((callback: Listener<T>) => {
         callback(value);
       });
@@ -45,25 +48,28 @@ export const useAtom = <T>(
   creator: T | Read<T>,
   link?: (source: T, local: T) => T,
 ) => {
-  const atomRef = useRef(createAtom<T>(creator)).current;
-  const lastLinkedState = useRef(getValueFromCreator(creator));
 
-  const creatorValue = getValueFromCreator(creator);
+  const creatorValue = useMemo(() => getValueFromCreator(creator), [creator]);
+  const lastLinkedState = useRef(creatorValue);
+  const linkFn = link ? useCallback((source: T, local: T) => link(source, local), [link]) : undefined
+  
+  const [_, setState] = useState(creatorValue);
+
+  const atomRef = useRef(createAtom<T>(
+    creator, 
+    setState
+  )).current;
+
 
   useMemo(() => {
-    if (lastLinkedState.current !== creatorValue && link) {
-      atomRef.store.value = link(creatorValue, atomRef.store.value);
+    if (lastLinkedState.current !== creatorValue && linkFn) {
+
+      atomRef.store.value = linkFn(creatorValue, atomRef.store.value);
 
       lastLinkedState.current = creatorValue;
-    }
-  }, [creatorValue, lastLinkedState, atomRef, link]);
 
-  const value = useSyncExternalStore(
-    (callback: Listener<T>) => atomRef.store.subscribe(callback),
-    () => atomRef.store.value,
-    () => atomRef.store.value,
-  );
-  atomRef.store.value = value;
+    }
+  }, [creatorValue, lastLinkedState, atomRef, linkFn]);
 
   return atomRef;
 };
