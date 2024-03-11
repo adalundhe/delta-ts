86a67c2656eddfda25da82a287933efd9720bff6
Ada Lundhe <ada@datavant.com>
2024-03-11 16:56:41 -0500

    AL: WIP readding useDerived

--- src/react/store.ts
+++ src/react/store.ts
@@ -1,6 +1,11 @@
-import { useMemo, useReducer, useRef } from "react";
-import { createBaseStore, isPromiseLike, use } from "~/base/store.ts";
-import { ReadWrite, StateStore } from "~/base/types.ts";
+import {
+  useMemo,
+  useReducer,
+  useRef,
+} from "react";
+import { createBaseStore, isPromiseLike, use } from "~/base/store";
+import { StateStore, ReadWrite } from "~/base/types.ts";
+
 
 const createStore = <T, U>(
   store: StateStore<T>,
@@ -8,65 +13,75 @@ const createStore = <T, U>(
   selector: (state: Awaited<T>) => U,
   comparator?: ({ next, prev }: { next: U; prev: U }) => boolean,
 ) => {
-  store.comparator = comparator as typeof store.comparator;
+  store.comparator = comparator as typeof store.comparator
   store.value = selector(
-    (isPromiseLike(init) ? use(init) : init) as Awaited<T>,
-  ) as unknown as T;
+      ( isPromiseLike(init) ? use(init) : init) as Awaited<T>
+    ) as unknown as T
 
-  return store as unknown as StateStore<U>;
-};
+  return store as unknown as StateStore<U>
+}
+
 
 const createInternalAtomReference = <T>(
-  atom: StateStore<T>,
-  init: T | ReadWrite<T>,
+    atom: StateStore<T>,
+    init: T | ReadWrite<T>
 ) => {
+  
   const useCreatedStore = <U>(
     selector: (state: Awaited<T>) => U,
     comparator?: ({ next, prev }: { next: U; prev: U }) => boolean,
-  ) => {
-    const atomRef = useRef(
-      createStore(atom, init, selector, comparator),
-    ).current;
+  ) => {  
 
-    const prev = useRef(atomRef.value);
+    const atomRef = useRef(createStore(
+      atom,
+      init,
+      selector,
+      comparator
 
-    const [value, rerender] = useReducer((_: U, next: U) => {
-      atomRef.value = next;
-      return next;
-    }, atomRef.value);
+    )).current
+
+    const prev = useRef(atomRef.value)
+
+    const [_, rerender] = useReducer((_: U, next: U) => {
+      atomRef.value = next
+      return next
+    }, atomRef.value)
 
     useMemo(() => {
       atomRef.subscribe((next) => {
-        if (!Object.is(prev.current, next)) {
-          prev.current = next as U;
+        if (!Object.is(prev.current, next)){
+          prev.current = next as U
           rerender(next as U);
         }
-      });
-    }, [atomRef]);
+      })
+    }, [atomRef])
 
-    return value as unknown as U;
+    atomRef.value = prev.current
+
+    return atomRef.value as unknown as U;
   };
 
   return useCreatedStore;
 };
 
+
 const createStoreFromState = () => {
-  const useCreatedStore = <U>(creator: ReadWrite<U>) => {
+  const useCreatedStore = <U>(
+    creator: ReadWrite<U>,
+  ) => {
     const store = createBaseStore({} as any);
 
     const setState = (next: Partial<U>): void => {
       store.set({
         ...store.get(),
-        ...next,
-      } as U);
+        ...next
+      } as U)
     };
 
-    const getState = (
-      selected?: StateStore<U>,
-    ): U extends PromiseLike<any> ? Awaited<U> : U =>
-      selected ? selected.get() : store.get();
+    const getState = (selected?: StateStore<U>): U extends PromiseLike<any> ? Awaited<U> : U => selected ? selected.get() : store.get();
 
     const init = creator(setState, getState as any);
+
 
     return createInternalAtomReference<U>(store, init);
   };
@@ -74,4 +89,42 @@ const createStoreFromState = () => {
   return useCreatedStore;
 };
 
-export const create = createStoreFromState();
+
+export const useDerived = <U>(
+  state: U,
+  link?: (source: U, local: U) => U
+) => {
+
+  const lastState = useRef(state)
+  const nextState = useRef(state)
+  const lastLinked = useRef(state)
+
+  useMemo(() => {
+    if (lastLinked.current !== state && link){
+      lastLinked.current = state
+      lastState.current = link(state, nextState.current)
+      nextState.current = lastState.current
+
+    }
+
+  }, [nextState, lastState, lastLinked, state])
+
+  const [_, rerender] = useReducer((prev: U, next: U) => {
+    lastState.current = prev
+    nextState.current = next
+    return next
+  }, state)
+
+
+  return [
+    nextState.current,
+    rerender
+  ] as [
+    U,
+    typeof rerender
+  ]
+  
+}
+
+export const create = createStoreFromState()
+
